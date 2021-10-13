d97077857d853660ee3f3bde6c8c331b6bf99d23
Patrick Ellis <319655+pje@users.noreply.github.com>
2021-10-13 12:21:49 -0400

    Use bats for bash testing (#8)

--- test/test.sh
+++ test/test.sh
@@ -1,44 +1,35 @@
-#!/bin/bash
+#!/usr/bin/env bats
 
-die() {
-  echo "$*" 1>&2
-  exit 1
+load '../node_modules/bats-assert/load'
+
+@test "Expressions CLI" {
+  run node dist/expressions/cli.js --pretty <test/expressions-input.json
+  assert_output <test/expressions.expected.json
 }
 
-# set -x # print each line as it's executed
-set -e # exit at the first error
+@test 'Templates CLI' {
+  run node dist/templates/cli.js --pretty <test/templates-input.json
+  assert_output <test/templates.expected.json
+}
 
-mkdir -p _temp
+@test "Templates CLI with unknown flag" {
+  run node dist/templates/cli.js --wat
+  assert_output --partial  "Error: Unknown option 'wat'"
+  assert_failure
+}
 
-echo 'Testing CLI'
-set +e
-# unknown flags should print usage and return error
-node dist/templates/cli.js --wat >_temp/cli-unknown-option.actual.json 2>&1
-[[ "$?" == 1 ]] || die "unexpected return status '$?'"
-grep -q "Error: Unknown option 'wat'" _temp/cli-unknown-option.actual.json
+@test "Templates CLI with --help" {
+  run node dist/templates/cli.js --help
+  assert_output --partial  "Usage:"
+  assert_failure # this should really exit 0
+}
 
-# `--help` should print usage
-node dist/templates/cli.js --help >_temp/cli-help.actual.json 2>&1
-grep -q "Usage:" _temp/cli-help.actual.json
-echo 'Success'
-set -e
+@test 'Templates CLI with --no-expand-expressions' {
+  run node dist/templates/cli.js --pretty --no-expand-expressions <test/templates-input.json
+  assert_output <test/templates.expected.json
+}
 
-echo 'Testing expressions'
-node dist/expressions/cli.js --pretty >_temp/expressions.actual.json 2>&1 <test/expressions-input.json
-diff test/expressions.expected.json _temp/expressions.actual.json
-echo 'Success'
-
-echo 'Testing template reader with --expand-expressions'
-node dist/templates/cli.js --pretty >_temp/templates.actual.json 2>&1 <test/templates-input.json
-diff test/templates.expected.json _temp/templates.actual.json
-echo 'Success'
-
-echo 'Testing template reader with --no-expand-expressions'
-node dist/templates/cli.js --pretty --no-expand-expressions >_temp/templates-no-expand.actual.json 2>&1 <test/templates-input.json
-diff test/templates.expected.json _temp/templates-no-expand.actual.json
-echo 'Success'
-
-echo 'Testing workflow parser'
-node dist/workflows/cli.js --pretty >_temp/workflow-parser.actual.json 2>&1 <test/workflow-parser-input.json
-diff test/workflow-parser.expected.json _temp/workflow-parser.actual.json
-echo 'Success'
+@test 'Workflows CLI' {
+  run node dist/workflows/cli.js --pretty <test/workflow-parser-input.json
+  assert_output <test/workflow-parser.expected.json
+}
