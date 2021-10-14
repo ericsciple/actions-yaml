09eb55fa09a1022344a0688388f748fcc306df4c
eric sciple <ericsciple@users.noreply.github.com>
2021-10-14 15:40:52 -0500

    include node_modules in release tarball

--- misc/release.sh
+++ misc/release.sh
@@ -3,5 +3,10 @@
 set -e
 mkdir -p _temp
 rm _temp/actions.yaml.tar.gz || true
+
+npm ci --production
+
 cd dist
+rm -rf node_modules || true
+cp -R ../node_modules ./
 tar czvf ../_temp/actions-yaml.tar.gz *
