bba2df134e4c68171abb19ff949f9928f88ed831
eric sciple <ericsciple@users.noreply.github.com>
2021-10-14 18:54:28 -0500

    update release script

--- misc/release.sh
+++ misc/release.sh
@@ -1,7 +1,19 @@
 #!/bin/bash
 
 set -e
+
+# Clean
+rm -rf _temp || true
+rm -rf dist || true
+rm -rf node_modules || true
 mkdir -p _temp
-rm _temp/actions.yaml.tar.gz || true
+
+# Build
+npm ci
+npm run build
+npm ci --production
+
+# Create tar.gz
 cd dist
+cp -R ../node_modules ./
 tar czvf ../_temp/actions-yaml.tar.gz *
